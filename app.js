d856c10ae45c8b888149b13cecdf078adb8c7247
Anudeepthi <anudeepthikolagani.999@gmail.com>
2026-06-01 20:49:49 +0530

    Added routes, controllers and services related to auth

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
-import uploadRoutes from "./src/routes/uploadRoutes.js";
-import fileRoutes from "./src/routes/fileRoutes.js";
+import uploadRoutes from "./src/routes/upload.routes.js";
+import fileRoutes from "./src/routes/file.routes.js";
+import authRoutes from "./src/routes/auth.router.js";
 
 const app = express();
 
+// Enable CORS for all routes
 app.use(cors());
+
+// Body parser middleware
 app.use(express.json());
 
 app.use("/api/v1/upload", uploadRoutes);
 app.use("/api/v1/files", fileRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 export default app;
